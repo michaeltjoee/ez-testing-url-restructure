aba7b521a0a0aa4aa6c041c0719bc84f52f658e9
michaeltjoee <113000037+michaeltjoee@users.noreply.github.com>
2025-12-08 16:45:18 +0700

    fix: tc 41-43 on production

--- tests/(41-43) - invalidPathRedirection.spec.ts
+++ tests/(41-43) - invalidPathRedirection.spec.ts
@@ -1,34 +1,52 @@
-import { test, expect } from '@playwright/test';
-import { BASE_URL, PAGE_PATHS, PAGE_EXPECTED_TEXT_EN } from './constants';
+import { test, expect } from "@playwright/test";
+import {
+  BASE_URL,
+  PAGE_PATHS,
+  PAGE_EXPECTED_TEXT_EN,
+  env,
+  PRODUCTION_EXPECTED_COUNTRY_CODE,
+  PRODUCTION_EXPECTED_CURRENCY,
+} from "./constants";
 
-const isUsingJapanVPN = process.env.isUsingJapanVPN === 'true';
-const shouldValidateText = process.env.validateText === 'true';
+const isUsingJapanVPN = process.env.isUsingJapanVPN === "true";
+const shouldValidateText = process.env.validateText === "true";
 
 const INVALID_LOCALE_COUNTRY_CASES = [
-  '/a-b',       // 1 digit
-  '/abc-abc',   // 3 digit
-  '/abcd-abcd', // 4 digit
+  "/a-b", // 1 digit
+  "/abc-abc", // 3 digit
+  "/abcd-abcd", // 4 digit
 ];
 
-const expectedLocale = isUsingJapanVPN ? 'en-us' : 'en-sg';
-const expectedCurrency = isUsingJapanVPN ? 'JPY' : 'SGD';
+const expectedCurrency =
+  env === "production"
+    ? PRODUCTION_EXPECTED_CURRENCY
+    : isUsingJapanVPN
+      ? "JPY"
+      : "SGD";
 
-const expectedUrl = isUsingJapanVPN
-  ? `${BASE_URL}/en-us?currency=JPY`
-  : `${BASE_URL}/en-sg`;
+const expectedUrl =
+  env === "production"
+    ? `${BASE_URL}/en-${PRODUCTION_EXPECTED_COUNTRY_CODE}`
+    : isUsingJapanVPN
+      ? `${BASE_URL}/en-us?currency=JPY`
+      : `${BASE_URL}/en-sg`;
 
-const expectedText = PAGE_EXPECTED_TEXT_EN[''];
+const expectedText = PAGE_EXPECTED_TEXT_EN[""];
 
-test.describe('TEST CASE: 41, 42, 43', () => {
+test.describe("TEST CASE: 41, 42, 43", () => {
   for (const invalidLocale of INVALID_LOCALE_COUNTRY_CASES) {
     for (const path of PAGE_PATHS) {
       const url = `${BASE_URL}${invalidLocale}${path}`;
 
-      test(`should redirect to ${expectedUrl} from ${url}`, async ({ page }) => {
+      test(`should redirect to ${expectedUrl} from ${url}`, async ({
+        page,
+      }) => {
         await page.goto(url);
 
         await expect(page).toHaveURL(expectedUrl);
-        await expect(page.locator(`text=${expectedCurrency}`).first()).toBeVisible();
+        await expect(
+          page.locator(`text=${expectedCurrency}`).first()
+        ).toBeVisible();
 
         if (shouldValidateText) {
           await expect(page.getByText(expectedText).first()).toBeVisible();
